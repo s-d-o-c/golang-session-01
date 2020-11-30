c21dbb8e6dce97dbf4a4fd8560a97c5756ee0196
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-11-30 07:21:14 +0700

    learning concurrency step-01.07/channel (the go workshop 16.07)

---
+++ concurrent/concurrent-work/Exercise16.07/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+	var s bytes.Buffer
+	log.SetOutput(&s)
+	log.SetFlags(0)
+
+	main()
+
+	output := s.String()
+	ln := len(output)
+	res := output[ln-5 : ln-1]
+
+	if res != "5050" {
+		t.Errorf("Expected 5050 but received %s", res)
+	}
+}
+
+/**
+
+Fix Inconsistent Result in Exercise16.06
+
+**/
