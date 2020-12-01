81fd770d44fa061df83f0057d2dc43bb8b532a72
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-12-01 15:14:01 +0700

    learning concurrency step-activity 01.01/channel (the go workshop activity 16.01)

---
+++ concurrent/concurrent-work/Activity16.01/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+
+	var s bytes.Buffer
+	log.SetOutput(&s)
+	log.SetFlags(0)
+
+	main()
+
+	// 90 numbers of two digits |xx| and 9 numbers of 1 digit |9| plus newline \n
+	expectedLength := (3 * 9) + (4 * 91) + 2
+
+	if len(s.String()) != expectedLength {
+		t.Errorf("Expecting length of %d but received %d", expectedLength, len(s.String()))
+	}
+
+	res := s.String()
+	for i := 1; i <= 100; i++ {
+		str := fmt.Sprintf("|%d|", i)
+		expectedLength -= len(str)
+		res = strings.ReplaceAll(res, fmt.Sprintf("|%d|", i), "")
+
+		if len(res) != expectedLength {
+			t.Errorf("Expected %d but received %d", expectedLength, len(res))
+		}
+	}
+}
